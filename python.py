6602643a87c95411b7d5afc0822ecbd6ee64414f
gptcs1 <sanjuganiga9@gmail.com>
2024-09-05 15:28:41 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,8 @@
+number = int(input('Enter a number: '))
+
+# check if number is greater than 0
+if number > 0:
+    print(f'{number} is a positive number.')
+
+print('A statement outside the if statement.')
+
